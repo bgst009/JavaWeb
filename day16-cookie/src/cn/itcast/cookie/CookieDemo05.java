91d6295f5803fb27e312dec86de6bf76b1f57d0a
bgst009 <1499135121@qq.com>
2020-03-08 17:39:46 +0800

    Cookie快速入门      发送多个cookie

---
+++ day16-cookie/src/cn/itcast/cookie/CookieDemo05.java
@@ -0,0 +1,19 @@
+package cn.itcast.cookie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/CookieDemo05")
+public class CookieDemo05 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
