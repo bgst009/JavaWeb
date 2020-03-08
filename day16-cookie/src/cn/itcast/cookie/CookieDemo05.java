3a6ea43a54f2ef4a374b22287c8e8c8af78afc9d
bgst009 <1499135121@qq.com>
2020-03-08 17:48:31 +0800

    Cookie快速入门      一个tomcat服务器中，部署了多个web项目中共享cookies

--- day16-cookie/src/cn/itcast/cookie/CookieDemo05.java
+++ day16-cookie/src/cn/itcast/cookie/CookieDemo05.java
@@ -2,15 +2,24 @@ package cn.itcast.cookie;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//一个tomcat服务器中，部署了多个web项目中共享cookies
+
 @WebServlet("/CookieDemo05")
 public class CookieDemo05 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.创建Cookie对象
+        Cookie c1 = new Cookie("msg","你好");
+        //设置path，让当前服务器下部署的所有项目共享Cookie信息
+        c1.setPath("/");
 
+        //3.发送Cookie
+        response.addCookie(c1);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
